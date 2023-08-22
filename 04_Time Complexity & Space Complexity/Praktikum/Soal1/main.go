1f6d4aa7d61270a37bc622da646bd7407735bb42
Anggi Pohontu <anggipohontu378@gmail.com>
2023-08-22 17:16:09 +0800

    add task

---
+++ 04_Time Complexity & Space Complexity/Praktikum/Soal1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(prima(1000000007)) //true
+	fmt.Println(prima(13)) //true
+	fmt.Println(prima(17)) //true
+	fmt.Println(prima(20)) //false
+	fmt.Println(prima(35)) //false
+	fmt.Println(prima(2)) //true
+	fmt.Println(prima(2000000007)) //false
+	fmt.Println(prima(97)) //true
+}
+
+func prima(number int) bool {
+	// Uji faktor prima hingga akar kuadrat dari number
+	for i := 2; i*i <= number; i++ {
+		if number%i == 0 || number%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
