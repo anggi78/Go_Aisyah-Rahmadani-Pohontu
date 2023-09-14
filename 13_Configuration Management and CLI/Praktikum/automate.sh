f474fb8cdcdb10050f85886bc1431f8ace6b9660
Anggi Pohontu <anggipohontu378@gmail.com>
2023-09-15 01:20:51 +0800

    add task cli

--- 13_Configuration Management and CLI/Praktikum/automate.sh
+++ 13_Configuration Management and CLI/Praktikum/automate.sh
@@ -1,6 +1,3 @@
-username_facebook="$aisyah"
-username_linkedin="$aisyahrahmadanipohontu"
-
 current_time=$(date "+%a %b %d %T %Z %Y")
 main_dir="aisyah at $current_time"
 
@@ -9,8 +6,8 @@ mkdir -p "$main_dir"
 mkdir -p "$main_dir/about_me/personal"
 mkdir -p "$main_dir/about_me/professional"
 
-echo "Facebook: $username_facebook" > "$main_dir/about_me/personal/facebook.txt"
-echo "LinkedIn: $username_linkedin" > "$main_dir/about_me/professional/linkedin.txt"
+echo "Facebook: https://www.facebook.com/aisyahfb" > "$main_dir/about_me/personal/facebook.txt"
+echo "LinkedIn: https://www.linkedin.com/in/aisyahrahmadanipohontulinkedin" > "$main_dir/about_me/professional/linkedin.txt"
 
 mkdir -p "$main_dir/my_friends"
 curl -o "$main_dir/my_friends/list_of_my_friends.txt" https://gist.githubusercontent.com/tegarimansyah/e91f335753ab2c7fb12815779677e914/raw/94864388379fecee450fde26e3e73bfb2bcda194/list%2520of%2520my%2520friends.txt
